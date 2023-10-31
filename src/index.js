e82e613b64bbb33edcb449915286c191127a676d
Imran97 <muhammad.imran@gainimpact.nl>
2023-10-31 15:17:46 +0500

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Radium-test">
     <App />
   </BrowserRouter>
 );
